68a7ab629a027c58e3b14866c7266dc1884c6025
anyel-ec <paulangel826@gmail.com>
2023-11-06 17:44:01 -0500

    add dark mode UI and icm activity

--- app/src/main/java/com/anyel/myfirstproject/MenuActivity.kt
+++ app/src/main/java/com/anyel/myfirstproject/MenuActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.AppCompatButton
 import com.anyel.myfirstproject.firstaApp.FirstAppActivity
+import com.anyel.myfirstproject.icmcalculator.ImcCalculatorActivity
 
 class MenuActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -13,11 +14,19 @@ class MenuActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
         val btnFirstApp = findViewById<AppCompatButton>(R.id.btnFirstApp)
+        val btnICM = findViewById<AppCompatButton>(R.id.btnICM)
         // saludar app
         btnFirstApp.setOnClickListener {
             // ir a la primera app
             navigateToSaludarApp()
         }
+        btnICM.setOnClickListener {  navegateToICM() }
+    }
+
+    private fun navegateToICM() {
+    val intent = Intent(this, ImcCalculatorActivity::class.java)
+       startActivity(intent)
+
     }
 
     private fun navigateToSaludarApp() {
---
+++ app/src/main/java/com/anyel/myfirstproject/icmcalculator/ImcCalculatorActivity.kt
@@ -0,0 +1,12 @@
+package com.anyel.myfirstproject.icmcalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.anyel.myfirstproject.R
+
+class ImcCalculatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_imc_calculator)
+    }
+}
